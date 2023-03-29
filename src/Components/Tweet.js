d86126587985b78ec1b988080050b9d060314636
DonnieAndMooie <owenjeaves@gmail.com>
2023-03-29 22:51:35 +0100

    Fix problem with profile pic on tweet

--- src/Components/Tweet.js
+++ src/Components/Tweet.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Comment from '../images/comment.png'
 import Retweet from '../images/retweet.png'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function Tweet({text, author}) {
+  let profilePic
+  if (author.picture){
+    profilePic = <img src={author.picture} alt="Profile" />
+  }
+  else{
+    profilePic = <AccountCircleIcon className='profile-pic'></AccountCircleIcon>
+  }
+
   return (
     <div className='tweet-item'>
-        <img src={author.picture} alt="Profile" />
+        {profilePic}
         <div className="author">
           <p className='name'>{author.name}</p>
           <p className='username'>@{author.username}</p>
